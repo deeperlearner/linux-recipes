9b4cb715b9d1795227daef4aa2a9b5d8f3794cfc
deeperlearner <b04202035@g.ntu.edu.tw>
2021-02-21 11:05:11 +0800

    .

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 This recipe is an automated procedures that install packages and setup coding environment.
 
 # Execution
-run `./chef.sh` and the chef will serve you yammy dishes. :)
+1. change config in `./link-dotfiles.sh`, server or client
+1. run `./chef.sh` and the chef will serve you yammy dishes. :)
+1. local change for `~/.bashrc`
--- install-packages.sh
+++ install-packages.sh
@@ -10,16 +10,16 @@ sudo apt-get install git
 pip3 install getgist
 
 # VIM
-git clone https://github.com/vim/vim.git
-cd vim/src
-make
-sudo make install
-cd ../../
-rm -rf vim
+# git clone https://github.com/vim/vim.git
+# cd vim/src
+# make
+# sudo make install
+# cd ../../
+# rm -rf vim
 
 # ranger
 sudo apt-get install ranger
-pip3 install ueberzug
+# pip3 install ueberzug
 
 # tmux
 sudo apt-get install tmux
