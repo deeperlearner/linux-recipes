8693f3f3e06135f293909a6b3744e2b8897ba54d
deeperlearner <b04202035@g.ntu.edu.tw>
2021-02-21 11:11:32 +0800

    .

--- get-github-files.sh
+++ get-github-files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/deeperlearner/Pytorch-Template.git ../
-git clone https://github.com/deeperlearner/dotfiles.git ../
+git clone https://github.com/deeperlearner/Pytorch-Template.git
+mv Pytorch-Template ../
+git clone https://github.com/deeperlearner/dotfiles.git
+mv dotfiles ../
